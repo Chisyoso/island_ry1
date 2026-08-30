c05a386c805eb0efbafc724ef6aee15e3b19dce3
CHM <132416621+Chisyoso@users.noreply.github.com>
2026-08-30 02:11:35 -0400

    Refactor tree initialization and player movement logic
    Adjusted initial random values for tree count and y-coordinate range. Improved player movement and interaction with objects.

--- code.cpp
+++ code.cpp
@@ -1,12 +1,21 @@
-#include "raylib.h" // raylib se vende por separado
+#include "raylib.h"
 #include <iostream>
 #include <string>
 #include <vector>
 #include <random>
 using namespace std;
+int hand = -1;
+
+int mision = 0;
+string pedido = "madera" ;
+
 random_device g;
+int frame[4];
 mt19937 gen(g());
 
+    int rx = 0;
+    int ry = 0; 
+    void hitbos(int x, int y, int dx, int dy);
 int aleatorio(int a, int b){
     uniform_int_distribution<int> total (a, b);
     return total(gen);
@@ -16,11 +25,14 @@ struct obj{
   int id;  
   int x;
   int y;
-  int t;  
+  int t;
+  int v, vx;  
 };
 vector<obj> arb;
 
 Color colo();
+
+// TAMAÑO DISPLAY
 const int tamay = 20;
 const int tamax = 20; 
 Color colores_matriz[tamax][tamay];
@@ -33,14 +45,52 @@ return a * 255 / 100;
     return 0;
 }
 
+void manos(){
+    switch(hand){
+        case 100: DrawRectangle(rx, ry + 25, 20, 20, BROWN); 
+        break; // madera
+        case 102: DrawRectangle(rx, ry + 25, 20, 20, GRAY);
+        break; // piedra
+        case 103: DrawRectangle(rx, ry + 25, 20, 20, RED);
+        break; // vallas salvajes
+    }
+}
+
+void bote(int x, int y){
+    Color cafe1 = {con(80), con(80), con(30), 255};
+    Color cafe2 = {con(70), con(70), con(20), 255};
+    for(int i = 0; i < 6; i++){
+    DrawRectangle(-850 + x, -30 + (10 * i) + y, 100, 20, cafe1);
+}
+    DrawRectangle(-840 + x, -30+ y, 10, 60, cafe2);
+    DrawRectangle(-760 + x , -30 + y, 10, 60, cafe2);
+    
+    
+    if(rx  < (-800 + x)+ 100 && rx >  (-800 + x)  && ry > (-50 + y)   && ry <  (-30 + y) + 100 ){
+    DrawText(TextFormat("consigue: %s    materiales: %d / 10", pedido.c_str(), mision ), rx- 200 , ry  - 50 , 20, BLACK);
+}
+}
+
+void lifeline(int min,  int max){
+    for(int i = 0; i < max; i++){
+        DrawRectangle(rx, ry + 30, 10 * i, 10, GRAY);
+    }
+    for(int i = 0; i < min; i++){
+        DrawRectangle(rx, ry + 30, 10 * i, 10, RED);
+    }
+    
+}
+
 void iniciar(){
-    int ctd_arb = aleatorio(10, 20);
+    int ctd_arb = aleatorio(5, 10);
     for(int i = 0; i < ctd_arb; i++){
         arb.push_back({});
         arb[i].x = aleatorio(-650, 650);
-        arb[i].y = aleatorio(-650, 650);
+        arb[i].y = aleatorio(-500, 500);
         arb[i].t = aleatorio(80, 170);
         arb[i].id = i;
+        arb[i].v = arb[i].t / 25;
+        arb[i].vx = arb[i].v; 
     }
     
     for(int a = 0; a < tamax; a++){
@@ -50,10 +100,36 @@ void iniciar(){
     }
 }
 
+void hitbos(int x, int y, int dx, int dy){
+    DrawLine(x, y, x, dy, RED);
+    DrawLine(x, y, dx ,  y, RED);
+    DrawLine(dx, dy, x, dy , RED);
+    DrawLine(dx, dy,dx , y , RED);
+}
+
 void arbol(int x, int y, int t, int id){
     DrawRectangle(x - t/2, y + t , t - t/2, t, BROWN);
     DrawRectangle(x- t/1.5, y , (t / 2.5) * 2, (t / 1.5) * 2, GREEN);
     DrawRectangle(x  -  t/1.1 , y + t/9 , (t / 1.5) * 2, (t / 2) * 2, GREEN);
+    
+    if(rx  < x + 100 && rx > x - 150  && ry < y + 200 + t && ry > y + 100 && (hand == id || hand == -1)){
+        hand = id;
+        lifeline(arb[id].v ,arb[id].vx);
+        frame[0] = 0;
+        if (IsKeyDown(KEY_Q) && frame[1] > 40 && hand < 99){ arb[id].v -= 1; frame[1] = 0;}  
+        DrawText(TextFormat("un arbol? toca (q) " ) , rx,  ry + 20, 20, BLACK);
+    }
+    else if(frame[0] > 40 && hand < 99){
+        hand  = -1;
+    }
+    if(arb[id].v <= 0){
+        arb.erase(arb.begin() + id);
+        for(int i = 0; i < arb.size(); i++){
+         arb[i].id = i;
+         hand = 100;   
+        }
+    }
+    
 }
 
 void map(int ax, int ay){
@@ -95,17 +171,19 @@ Color colo(){
         default: return c2;
     }
 }
+
 void arbolrend(){
     for(int i = 0; i < arb.size(); i++){
+        arb[i].id = i;
         arbol(arb[i].x,arb[i].y, arb[i].t, arb[i].id);
-    }    
+    }
 }
 
 int main() {
    // #960
-    int speed = 5;
-    int jx = 0;
+   int jx = 0;
     int jy = 0;
+    int speed = 5;
     iniciar();
     int life = 8;
     
@@ -121,11 +199,31 @@ int main() {
     SetTargetFPS(60);
 
     while (!WindowShouldClose()){
-
-        if (IsKeyDown(KEY_RIGHT)){ jugador.x += speed; jx +=speed;}
-        if (IsKeyDown(KEY_LEFT)){  jugador.x -= speed; jx -=speed;}
-        if (IsKeyDown(KEY_UP)){    jugador.y -= speed; jy +=speed;}
-        if (IsKeyDown(KEY_DOWN)){  jugador.y += speed; jy -=speed;}
+    if(frame[0] < 60){
+        frame[0]++;
+    }
+    if(frame[1] < 60){
+        frame[1]++;
+    }
+        if (IsKeyDown(KEY_RIGHT)){ jugador.x += speed; jx +=speed; rx += speed;}
+        if (IsKeyDown(KEY_LEFT)){  jugador.x -= speed; jx -=speed; rx -= speed;}
+        if (IsKeyDown(KEY_UP)){    jugador.y -= speed; jy +=speed; ry -= speed;}
+        if (IsKeyDown(KEY_DOWN)){  jugador.y += speed; jy -=speed; ry += speed;}
+        // funciones 
+        if(rx > 800 ){
+            jugador.x -= 4; jx -= 4; rx -= 4;
+        }
+        if(rx < -800){
+            jugador.x += 4; jx += 4; rx += 4;
+        }
+        if(ry < -800){
+            jugador.y += 4; jy -=4; ry += 4;
+            
+        }
+        if(ry > 800){
+            jugador.y -= 4; jy +=4; ry -= 4;
+        }
+        
         BeginDrawing();
        camera.target = jugador;
               BeginMode2D(camera);
@@ -140,11 +238,14 @@ int main() {
         }
         x += 400;
     }
+        bote(0,0);
         per(jugador.x - 25,jugador.y - 25);
+        manos();
         arbolrend();
         EndMode2D();
         DrawText("coordenadas : ", 10, 10, 20, BLACK);
         DrawText(TextFormat("x: %d y: %d", jx , jy) , 10, 30, 20, BLACK);
+        DrawText(TextFormat("x: %d y: %d", rx , ry) , 10, 50, 20, BLACK);
          x = 0;
         for(int i = 0; i < life; i++){
             DrawRectangle(340 + x, 350, 10, 10, RED);
